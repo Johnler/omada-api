0cc079e9fdb6513ec416ebd223d481b1b82a0d66
Johnler <johnlerwonggamboa@gmail.com>
2026-09-19 01:06:19 +0800

    Update

--- src/model/HotspotInterfaceModel.ts
+++ src/model/HotspotInterfaceModel.ts
@@ -20,29 +20,30 @@ class HotspotInterfaceModel {
   };
 
   createVoucher = async (payload: any) => {
-    const { cookie, token, controllerId, siteKey } = payload;
+    const { cookie, token, controllerId, siteKey, minutes } = payload;
     const data = {
       codeLength: 6,
       amount: 1,
       type: 0,
-      downLimitEnable: true,
-      downLimit: 51200,
-      downLimitUnit: 1,
-      upLimitEnable: true,
-      upLimit: 51200,
-      upLimitUnit: 1,
-      trafficLimitEnable: false,
-      trafficLimit: null,
-      durationType: 0,
-      duration: 60,
+      durationType: 1,
+      duration: minutes, // Duration in minutes
       note: null,
       maxUsers: 1,
       portals: [`${siteKey}`],
+      // downLimitEnable: true,
+      // downLimit: 51200,
+      // downLimitUnit: 1,
+      // upLimitEnable: true,
+      // upLimit: 51200,
+      // upLimitUnit: 1,
+      // trafficLimitEnable: false,
+      // trafficLimit: null,
     };
     httpClient.defaults.headers.common["CSRF-TOKEN"] = token;
     httpClient.defaults.headers.common["Cookie"] = cookie;
-    const response = await httpClient.get(
-      `/${controllerId}/api/v2/hotspot/sites/${siteKey}/vouchers`
+    const response = await httpClient.post(
+      `/${controllerId}/api/v2/hotspot/sites/${siteKey}/vouchers`,
+      data
     );
     
     return response;
